ec9c3185e424564613fa169900e2009f32488fbb
hbessala <herv.bessala@gmail.com>
2024-04-29 21:14:14 +0000

    Updates bakup.sh file

--- backup.sh
+++ backup.sh
@@ -3,6 +3,6 @@
 NOW="$(date +"%m-%d-%Y")"
 FILE="backup.$NOW.tar.gz"
 
-tar cvf /home/control/work/$FILE
+tar cvf /home/control/work/$FILE .
 
 #scp /home/control/work/$FILE webapp@46.101.209.213:/home/webapp/backups
